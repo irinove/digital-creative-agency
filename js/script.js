2b18a5eeb9fef731438bb87304e612e76ae81e5d
Irin Sultana Ove <45695615+irinove@users.noreply.github.com>
2019-06-19 22:44:03 +0600

    basic js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+ //  JavaScript Basics
+ /*   JavaScript Basics 
+    lorem	 JavaScript Basics JavaScript Basics
+ */
+
+
+ var myVariable = "Welcome To JavaScript"
+ // document.write(myVariable);    
+ // alert(myVariable);
+
+ var a = 20;
+
+ if(a== 10) {
+ 	 document.write("a value is 10")
+ } else {
+ 	document.write("a value is not 10"); 
+ }
+
+ function myFunction() {
+ 		document.write("<br> code execution of function.");
+ }
+
+ myFunction(); 
+
+ var person = {
+ 	firstName: "Irin",
+ 	lastName: "Sultana",
+ 	height: 5.3,
+ 	age: 23
+ }
+
+document.write("<br>" + person.firstName + "<br>");
+document.write(person.lastName + "<br>");
+document.write(person.height + "<br>");
+document.write(person.age + "<br>");
